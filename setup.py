0c7567dc34b4c3310fccf3342adf35b11aabf501
Gavin M. Roy <gavinr@aweber.com>
2015-09-23 16:58:55 -0400

    Fix the installation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='sprockets.mixins.amqp',
-    version='0.1.0',
+    version='0.1.1',
     description='Mixin for publishing events to RabbitMQ',
     long_description=open('README.rst').read(),
     url='https://github.com/sprockets/sprockets.mixins.amqp',
